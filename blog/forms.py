bca624658d1bf4e1ff6b440a39fc63db05fdabb5
Sherzod <sherzik99@gmail.com>
2021-07-05 00:34:39 +0500

    views changes in post detail

---
+++ blog/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
+    
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs = {'placeholder': 'Enter name', 
+                                            'class' : 'form-control'}
+        self.fileds['email'].widget.attrs = {'placeholder': 'Enter email', 
+                                             'class': 'form-control'}
+        self.fields['body'].widget.attrs = {'placeholder': 'Comment here...',
+                                            'class': 'form-control',
+                                            'rows': '5'}
+
+
+
+                                            
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import CommentForm
 
 def post_list(request):
     posts = Post.published.all()
@@ -19,6 +20,20 @@ def post_list(request):
 
 def post_detail(request, post):
     post = get_object_or_404(Post, slug=post, status='published')
+    
+    comments = post.comments.filter(acitve=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            return redirect(post.get_absolute_url() + '#' + str(new_comment.id))
+        else:
+            comment_form = CommentForm()
+
     return render(request, 'blog/post_detail.html', {'post':post})
 
 
